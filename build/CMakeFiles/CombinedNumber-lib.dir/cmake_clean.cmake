7c964509e9b268c2e9e577ed3339ec1179437704
piotr.koter <piotrkoter@gmail.com>
2018-08-14 21:34:49 +0200

    First commit with Travis

---
+++ build/CMakeFiles/CombinedNumber-lib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CombinedNumber-lib.dir/src/CombinedNumber.cpp.o"
+  "libCombinedNumber-lib.pdb"
+  "libCombinedNumber-lib.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CombinedNumber-lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/CombinedNumber.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CombinedNumber.dir/src/main.cpp.o"
+  "CombinedNumber.pdb"
+  "CombinedNumber"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CombinedNumber.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
